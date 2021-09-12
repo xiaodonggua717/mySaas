8d8ca9dfc82f8e79999322ff5e84f5c251afacae
xiaodonggua717 <904674468@qq.com>
2021-09-12 12:30:47 +0800

    完成了员工详情页,完成了员工用户名和密码的修改

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -6,15 +6,15 @@
           <!-- 登录账户设置 -->
           <el-tab-pane label="登录账户设置">
             <!-- 放置表单 -->
-            <el-form label-width="120px" style="margin-left: 120px; margin-top:30px">
-              <el-form-item label="姓名:">
-                <el-input style="width:300px" />
+            <el-form ref="userForm" label-width="120px" style="margin-left: 120px; margin-top:30px" :model="userInfo" :rules="rules">
+              <el-form-item label="姓名:" prop="username">
+                <el-input v-model="userInfo.username" style="width:300px" />
               </el-form-item>
-              <el-form-item label="密码:">
-                <el-input style="width:300px" type="password" />
+              <el-form-item label="密码:" prop="password2">
+                <el-input v-model="userInfo.password2" style="width:300px" type="password" />
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
+                <el-button type="primary" @click="saveUser">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -31,8 +31,44 @@
 </template>
 
 <script>
+import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      userId: this.$route.params.id, // 这样可以后面直接通过 this.userId进行获取数据
+      userInfo: {
+        //   专门存放基本信息
+        username: '',
+        // 接口中读取的是后端的密文，并不能解密，所以没有任何修改就保存时，会校验失败，因为密文超过了规定的12位长度，
+        // 为了真的修改密码，设定了一个临时的字段password2，用它来存储修改值，最后保存的时候，把password2传给password
+        password2: ''
+      },
+      rules: {
+        username: [{ required: true, message: '姓名不能为空', trigger: 'blur' }],
+        password2: [{ required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }]
+      }
+    }
+  },
+  created() {
+    this.getUserDetailById()
+  },
+  methods: {
+    async getUserDetailById() {
+      this.userInfo = await getUserDetailById(this.userId)
+    },
+    async saveUser() {
+      try {
+        // 校验
+        await this.$refs.userForm.validate()
+        await saveUserDetailById({ ...this.userInfo, password: this.userInfo.password2 }) // 将新密码的值替换原密码的值
+        this.$message.success('保存成功')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
 
